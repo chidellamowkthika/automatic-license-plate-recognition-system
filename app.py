f89a215e21a7ccbff665431e52f6eb7266c09a19
chidella chakri <chidellamowkthika@gmail.com>
2022-04-04 12:42:21 +0530

    browsed the car image

--- app.py
+++ app.py
@@ -1,2 +1,26 @@
 import streamlit as st
+from PIL import Image 
+import os
+
 st.header("licence number plate recognition system")
+src_file=st.file_uploader("upload car image",type=["png","jpg","jpeg"])
+def load_image(image_file):
+    img=Image.open(image_file)
+    return img
+
+if src_file is not None:
+    file_details={
+        "filename": src_file.name,
+        "filetype": src_file.type,
+        "filesize": src_file.size
+    }
+    st.write(file_details)
+    st.image(load_image(src_file),width=250)
+
+    with open(os.path.join("inputs","car.jpg"),"wb") as f:
+        f.write(src_file.getbuffer())
+    
+    
+
+
+
